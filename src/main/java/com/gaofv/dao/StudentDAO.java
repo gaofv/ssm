24c8d571520c57a7dd65bf7aafbbe3eb53948f1e
gaofei32419 <gaofei32419@hundsun.com>
2020-08-06 12:40:33 +0800

    修改/com/gaofv/dao/StudentDAO.java

--- src/main/java/com/gaofv/dao/StudentDAO.java
+++ src/main/java/com/gaofv/dao/StudentDAO.java
@@ -2,4 +2,14 @@ public interface StudentDAO{
 	public static void main(String args[]){
 		System.out.println();
 	}
+	
+	public static void main(String args[]){
+		System.out.println();
+	}
+	public static void main(String args[]){
+		System.out.println();
+	}
+	public static void main(String args[]){
+		System.out.println();
+	}
 }
